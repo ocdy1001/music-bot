2d2dbe1381907753430424c8e98aade0b43f7c0a
Cody Bloemhard <cblmhrd@live.nl>
2021-01-12 00:07:35 +0100

    alive, help and notes command now working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use music_gen::*;
+use music_gen::theory::*;
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
 use serenity::model::channel::Message;
@@ -11,7 +13,7 @@ use serenity::framework::standard::{
 };
 
 #[group]
-#[commands(echo)]
+#[commands(alive, help, notes)]
 struct General;
 
 struct Handler;
@@ -39,9 +41,46 @@ async fn main() {
     }
 }
 
+fn break_up(msg: String) -> Vec<String>{
+    if msg.len() <= 1000 { vec![msg] }
+    else {
+        let mut res = Vec::new();
+        let mut string = String::new();
+        let mut l = 0;
+        for (i, c) in msg.chars().enumerate(){
+            string.push(c);
+            if i - l >= 1000 && c == '\n' {
+                res.push(string);
+                string = String::new();
+                l = i;
+            }
+        }
+        res.push(string);
+        res
+    }
+}
+
 #[command]
-async fn echo(ctx: &Context, msg: &Message) -> CommandResult {
-    let echo = format!("Echo: \"{:?}\"", msg.content);
-    msg.reply(ctx, &echo).await?;
+async fn alive(ctx: &Context, msg: &Message) -> CommandResult{
+    msg.reply(ctx, &"I   A M   A L I V E").await?;
+    Ok(())
+}
+
+#[command]
+async fn help(ctx: &Context, msg: &Message) -> CommandResult{
+    let empleh = &"Usage:\n\t~notes <comma separated notes>\n\t\texamples:\n\t\t\t~notes a,c,e,g\n\t\t\t~notes As,Db,Fb,g";
+    msg.reply(ctx, empleh).await?;
+    Ok(())
+}
+
+#[command]
+async fn notes(ctx: &Context, msg: &Message) -> CommandResult{
+    let prepared = msg.content.clone().split(' ').into_iter().skip(1).collect::<String>();
+    println!("{}", prepared);
+    let test_outp = notes_analysis(prepared, ChordStyling::Std);
+    let broken = break_up(test_outp);
+    for piece in broken{
+        msg.reply(ctx, &piece).await?;
+    }
     Ok(())
 }
