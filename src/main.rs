4272c226706f9d71a58eb4296ed3ba39462ae9b4
Cody Bloemhard <cblmhrd@live.nl>
2021-01-11 21:29:25 +0100

    simple bot

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+#[group]
+#[commands(echo)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let args: Vec<String> = std::env::args().collect();
+    let mut client = Client::builder(&args[1])
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn echo(ctx: &Context, msg: &Message) -> CommandResult {
+    let echo = format!("Echo: \"{:?}\"", msg.content);
+    msg.reply(ctx, &echo).await?;
+    Ok(())
+}
